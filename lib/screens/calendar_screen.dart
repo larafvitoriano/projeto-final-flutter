9d71ade4776bba86632ab4f2d8b4305d8b68f02d
Lara Fernanda <larafvitoriano@gmail.com>
2025-04-04 19:16:07 -0300

    refac: ajustando calendar screen

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -12,134 +12,87 @@ class _CalendarScreenState extends State<CalendarScreen> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
-  Map<DateTime, List<String>> _events = {}; // Mapa para armazenar eventos
-  TextEditingController _eventTitleController = TextEditingController();
-  TextEditingController _eventDescriptionController = TextEditingController();
-  DateTime _selectedEventDate = DateTime.now(); // Data do evento selecionada no popup
-
-  List<String> _getEventsForDay(DateTime day) {
-    return _events[day] ?? [];
-  }
-
-  void _addEvent(DateTime date, String eventTitle, String eventDescription) {
-    setState(() {
-      if (_events[date] == null) {
-        _events[date] = [];
-      }
-      _events[date]!.add('$eventTitle: $eventDescription');
-    });
-  }
-
-  void _showAddEventDialog(BuildContext context) async {
-    DateTime? pickedDate = await showDatePicker(
-      context: context,
-      initialDate: _selectedEventDate,
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
-
-    if (pickedDate != null) {
-      setState(() {
-        _selectedEventDate = pickedDate;
-      });
-
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Adicionar Lembrete'),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text('Data: ${DateFormat('dd/MM/yyyy').format(_selectedEventDate)}'),
-                TextField(
-                  controller: _eventTitleController,
-                  decoration: InputDecoration(labelText: 'Título'),
-                ),
-                TextField(
-                  controller: _eventDescriptionController,
-                  decoration: InputDecoration(labelText: 'Descrição'),
-                ),
-              ],
-            ),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: Text('Cancelar'),
-              ),
-              TextButton(
-                onPressed: () {
-                  _addEvent(
-                    _selectedEventDate,
-                    _eventTitleController.text,
-                    _eventDescriptionController.text,
-                  );
-                  _eventTitleController.clear();
-                  _eventDescriptionController.clear();
-                  Navigator.of(context).pop();
-                },
-                child: Text('Salvar'),
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calendário'),
+        title: Text('Calendário', style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.blue[300], // Cor da AppBar
+        elevation: 2,
       ),
-      body: Column(
-        children: <Widget>[
-          TableCalendar(
-            firstDay: DateTime.utc(2010, 10, 16),
-            lastDay: DateTime.utc(2030, 3, 14),
-            focusedDay: _focusedDay,
-            calendarFormat: _calendarFormat,
-            selectedDayPredicate: (day) {
-              return isSameDay(_selectedDay, day);
-            },
-            onDaySelected: (selectedDay, focusedDay) {
-              if (!isSameDay(_selectedDay, selectedDay)) {
-                setState(() {
-                  _selectedDay = selectedDay;
-                  _focusedDay = focusedDay;
-                });
-              }
-            },
-            onFormatChanged: (format) {
-              if (_calendarFormat != format) {
-                setState(() {
-                  _calendarFormat = format;
-                });
-              }
-            },
-            onPageChanged: (focusedDay) {
-              _focusedDay = focusedDay;
-            },
-            eventLoader: _getEventsForDay,
-            locale: 'pt_BR',
-          ),
-          Expanded(
-            child: StatefulBuilder(
-              builder: (BuildContext context, StateSetter setState) {
-                return ListView(
-                  children: _getEventsForDay(_selectedDay ?? _focusedDay)
-                      .map((event) => ListTile(title: Text(event)))
-                      .toList(),
-                );
-              },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Card( // Card para o calendário
+              elevation: 4,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TableCalendar(
+                  firstDay: DateTime.utc(2010, 10, 16),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  focusedDay: _focusedDay,
+                  calendarFormat: _calendarFormat,
+                  selectedDayPredicate: (day) {
+                    return isSameDay(_selectedDay, day);
+                  },
+                  onDaySelected: (selectedDay, focusedDay) {
+                    if (!isSameDay(_selectedDay, selectedDay)) {
+                      setState(() {
+                        _selectedDay = selectedDay;
+                        _focusedDay = focusedDay;
+                      });
+                    }
+                  },
+                  onFormatChanged: (format) {
+                    if (_calendarFormat != format) {
+                      setState(() {
+                        _calendarFormat = format;
+                      });
+                    }
+                  },
+                  onPageChanged: (focusedDay) {
+                    _focusedDay = focusedDay;
+                  },
+                  locale: 'pt_BR',
+                  headerStyle: HeaderStyle(
+                    formatButtonTextStyle: TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
+                    formatButtonDecoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    leftChevronIcon: Icon(Icons.chevron_left, color: Colors.blue),
+                    rightChevronIcon: Icon(Icons.chevron_right, color: Colors.blue),
+                  ),
+                  calendarStyle: CalendarStyle(
+                    selectedDecoration: BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
+                    ),
+                    selectedTextStyle: TextStyle(color: Colors.white),
+                    todayDecoration: BoxDecoration(
+                      color: Colors.blue.withValues(blue: 0.3),
+                      shape: BoxShape.circle,
+                    ),
+                    todayTextStyle: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  daysOfWeekStyle: DaysOfWeekStyle(
+                    weekdayStyle: TextStyle(fontWeight: FontWeight.bold),
+                    weekendStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.red),
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _showAddEventDialog(context),
-        child: Icon(Icons.add),
+        onPressed: () {},
+        child: Icon(Icons.add, color: Colors.white),
+        backgroundColor: Colors.blue,
       ),
     );
   }
